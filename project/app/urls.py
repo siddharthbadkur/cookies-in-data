08a6b90d4807476f5cc0985f64371b4cb681a9b5
siddharth <badkursiddharth282@gmail.com>
2024-09-09 23:16:47 +0530

    cookies

---
+++ project/app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('set/',set ,name='set'),
+    path('get/',get ,name='get')
+]
---
+++ project/app/views.py
@@ -0,0 +1,25 @@
+
+# Create your views here.
+from django.shortcuts import render 
+
+def set(request):
+  data = render(request,'set.html')
+  data.set_cookie('name','siddharth',max_age=5*24*60*60,httponly=True,secure=True)
+  data.set_cookie('age','23',max_age=3*24*60*60,httponly=True,secure=True)
+  data.set_cookie('city','bpl',max_age=1*24*60*60,httponly=True,secure=True) 
+  return data
+
+
+    
+
+def get(request):
+  print(request.COOKIES),
+  name=request.COOKIES['name'],
+  age=request.COOKIES['age'],
+  city=request.COOKIES['city']
+  data ={
+    'name':name,
+    'age':age,
+    'city':city
+}
+  return render(request,'get.html',data)
